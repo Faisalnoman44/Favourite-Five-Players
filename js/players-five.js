a7bdcd39569436f8a6b2250389d5f58f330008fc
Faisalnoman44 <mdnoman.mn11@gmail.com>
2022-08-22 17:21:14 +0600

    calculation fixed

--- js/players-five.js
+++ js/players-five.js
@@ -1,6 +1,5 @@
 const playerArray = []
 
-
 function display(players) {
     const selectedPlayer = document.getElementById('selected-player');
     selectedPlayer.innerText = '';
@@ -22,25 +21,45 @@ function display(players) {
 function addToSelect(element) {
     const playerName = element.parentNode.parentNode.children[0].innerText;
 
-    playerArray.push(playerName);
+    
+    if (playerArray.length > 4) {
+        alert('You have already selected five players');
+        return;
+    }
+    else{
+        playerArray.push(playerName);
+    }
+    
+        display(playerArray);
+
+        disable(element);
+    
 
 
-    display(playerArray);
-
-    disable(element);
 }
 
-document.getElementById('calculate-btn').addEventListener('click',function(){
+document.getElementById('calculate-btn').addEventListener('click', function () {
     const perPlayerCost = getInputValueById('player-cost');
-    
+    if(isNaN(perPlayerCost)){
+        alert('Please Provide a valid amount');
+        document.getElementById('player-cost').value = '';
+        return;
+    }
+
     const totalPlayerExpenses = perPlayerCost * playerArray.length;
 
-    setTextvalueById('player-expenses',totalPlayerExpenses);
+    setTextvalueById('player-expenses', totalPlayerExpenses);
 })
 
-document.getElementById('total-calculate').addEventListener('click', function(){
+document.getElementById('total-calculate').addEventListener('click', function () {
     const managerFee = getInputValueById('manager-fee');
     const coachFee = getInputValueById('coach-fee');
+    if(isNaN(managerFee) || isNaN(coachFee)){
+        alert('Please provide a valid amount');
+        document.getElementById('manager-fee').value = '';
+        document.getElementById('coach-fee').value = '';
+        return;
+    }
 
     const totalPlayer = document.getElementById('player-expenses');
     const totalPlayerExpensesString = totalPlayer.innerText;
@@ -48,5 +67,5 @@ document.getElementById('total-calculate').addEventListener('click', function(){
 
     const finalTotal = totalPlayerExpenses + managerFee + coachFee;
 
-    setTextvalueById('final-total',finalTotal)
+    setTextvalueById('final-total', finalTotal)
 })
